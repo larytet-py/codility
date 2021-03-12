0b4253b925dc5b4eb9391e7086226d3cdf7e22f1
arkady <arkady>
2021-03-12 18:45:56 +0200

    update

--- distinct-integers.py
+++ distinct-integers.py
@@ -1,7 +1,10 @@
 '''
-N integers in array of [1..N] In one move you inc/dec the value by 1
-After every move all number should remain within [1..N]
-What is the smallest number of steps to make all values distinct?
+You are given an array A of N integers within the range [1..N] 
+In one move, you increase or deccreas the value of any element by 1.
+After every move all numbers should remain within the range [1..N]
+Your task is to find the smallest required number of moves to make all elements 
+in the array pairwise distinct (i.e. no value can appear in the array more than once).
+
 [1,2,1] can be changed in 2 steps [1,2,2], [1,2,3]
 [6,2,3,5,6,3] 4 steps [6,2,1,5,3]
 '''
